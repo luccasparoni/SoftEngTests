841f10e2f9ff8c8bc9861a5d1409b61651ef673a
Carlos Henrique Lima Melara <charlesmelara@outlook.com>
2020-05-22 13:13:47 -0300

    Add test cases to Estudante and minor fixes to allow testing methods."

--- EstudanteTests.java
+++ EstudanteTests.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
 import org.junit.Before;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class EstudanteTests {
@@ -17,7 +18,49 @@ public class EstudanteTests {
 
     @Test
     public void casoTeste1 () {
-        Estudante teste = new Estudante("7654321");
+        Estudante teste = new Estudante("1234567");
         assertNotNull(teste);
+    }
+
+    @Test
+    public void casoTeste2() {
+        assertThrows(Error.class, () -> { new Estudante("123A567"); });
+    }
+
+    @Test
+    public void casoTeste3() {
+        assertThrows(Error.class, () -> { new Estudante("123"); });
+    }
+
+    @Test
+    public void casoTeste4() {
+        assertThrows(Error.class, () -> { new Estudante(""); });
+    }
+
+    @Test
+    public void casoTeste5() {
+        int erro = estudante.addNota("P1", 0.0);
+        assertEquals(0, erro);
+    }
+
+    @Test
+    public void casoTeste6() {
+        int erro = estudante.addNota("P2", 10.0);
+        assertEquals(10, erro);
+    }
+
+    @Test
+    public void casoTeste7() {
+        assertThrows(Error.class, () -> { estudante.addNota("P1", -1.0); });
+    }
+
+    @Test
+    public void casoTeste8() {
+        assertThrows(Error.class, () -> { estudante.addNota("P2", 11.0); });
+    }
+
+    @Test
+    public void casoTeste9() {
+        assertThrows(Error.class, () -> { estudante.addNota("P3", Double.parseDouble("a")); });
     }
 }
